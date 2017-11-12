c2637f518335bd1a9e3197bd32dae3d229a40137
Elvis <elvis@mapom.me>
2017-11-12 17:17:01 +0100

    vagrant box and bootstrap script for testing purpose

---
+++ vagrantstrap.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+set -e
+
+apt-get update
+apt-get upgrade -yyq
+apt-get install -yyq make jq
+
+wget -nv -O /tmp/go1.9.2.linux-amd64.tar.gz https://redirector.gvt1.com/edgedl/go/go1.9.2.linux-amd64.tar.gz
+tar -C /usr/local -xzf /tmp/go1.9.2.linux-amd64.tar.gz
+rm -rf /tmp/go1.9.2.linux-amd64.tar.gz
+echo 'PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/local/go/bin"' > /etc/environment
+
+adduser --system http-cmd
